5d3c4637dc3e843f90295d58d5df0f09b6f86b10
myruldeen <funstudy10@gmail.com>
2020-05-18 22:53:22 +0800

    fix indent

--- LabExercise3/Exercise2.java
+++ LabExercise3/Exercise2.java
@@ -14,7 +14,7 @@ public class Exercise2 {
     	System.out.print("Input the number of node: ");
     	
     	// Get the value from input
-		int nodeList = x.nextInt();
+	int nodeList = x.nextInt();
     	
     	// Initiate arrayList
     	List<Integer> list = new ArrayList<Integer>();
@@ -24,62 +24,62 @@ public class Exercise2 {
     		System.out.print("Input data for node "+(i + 1)+ ": ");
     		
     		int node = x.nextInt();
-			list.add(node);
+		list.add(node);
     	}
 
-		System.out.println("Data entered in the list are : ");
-		for(int i=0; i<list.size(); i++) {
-			System.out.println("Data = " + list.get(i));
-		}
+	System.out.println("Data entered in the list are : ");
+	for(int i=0; i<list.size(); i++) {
+		System.out.println("Data = " + list.get(i));
+	}
     	System.out.println("Total number of nodes= " + list.size());
     	
     	// Insert data at the end of the list
     	System.out.print("Input data to insert at the end of the list : ");
     	
     	// Get the value from input
-		int nd = x.nextInt();
-		list.add(nd);
-		System.out.println("Data, after inserted in the list are : ");
+	int nd = x.nextInt();
+	list.add(nd);
+	System.out.println("Data, after inserted in the list are : ");
 		
-		// Loop
-		for(int i=0; i<list.size(); i++) {
-			System.out.println("Data = " + list.get(i));
-		}
+	// Loop
+	for(int i=0; i<list.size(); i++) {
+		System.out.println("Data = " + list.get(i));
+	}
     	System.out.println("Total number of nodes= " + list.size());
     	
     	// Insert data at the middle of the list
     	System.out.print("Input data to insert in the middle of the list : ");
    
-		int middle = x.nextInt();
+	int middle = x.nextInt();
 
-		// Get position
+	// Get position
     	System.out.print("Input the position to insert new node :");
 
     	// Get the value from input
-		int position = x.nextInt();
+	int position = x.nextInt();
 		
-		// Add nth 
-		list.add(position, middle);
+	// Add nth 
+	list.add(position, middle);
 		
-		System.out.println("Insertion completed successfully.");
-		System.out.println("The new list are :");
-		// Loop data
-		for(int i=0; i<list.size(); i++) {
-			System.out.println("Data = " + list.get(i));
-		}
+	System.out.println("Insertion completed successfully.");
+	System.out.println("The new list are :");
+	// Loop data
+	for(int i=0; i<list.size(); i++) {
+		System.out.println("Data = " + list.get(i));
+	}
 			
-		System.out.println("Total number of nodes= " + list.size());
+	System.out.println("Total number of nodes= " + list.size());
 		
-		// Delete at first index
-		System.out.println("Data of node 1 which is being deleted is :"+list.get(0));
+	// Delete at first index
+	System.out.println("Data of node 1 which is being deleted is :"+list.get(0));
 	
     	list.remove(0);
     	System.out.println("The new list are :");
-		// Loop data
-		for(int i=0; i<list.size(); i++) {
-			System.out.println("Data = " + list.get(i));
-		}
-		System.out.println("Total number of nodes= " + list.size());
+	// Loop data
+	for(int i=0; i<list.size(); i++) {
+		System.out.println("Data = " + list.get(i));
+	}
+	System.out.println("Total number of nodes= " + list.size());
     }
     
 }
